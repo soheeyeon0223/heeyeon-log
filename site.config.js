15ef6bd1e3972ce06b0adfa8fc14b314473126d2
soheeyeon0223 <heeyeon0223@soongsil.ac.kr>
2025-07-19 21:03:51 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "heeyeon",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
     bio: "I develop everything using node.",
@@ -18,7 +18,7 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "heeyeon-log",
     description: "welcome to morethan-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
